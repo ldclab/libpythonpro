245b3d5a5778cf6e093e5d76132c11f4391af314
lucianodacunha <lucianodacunha.net@gmail.com>
2018-05-23 20:09:09 -0300

    Injeção de Dependências
    Correção exceções flake8

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -32,6 +32,7 @@ def test_qde_de_spam(sessao, usuarios):
 
     assert len(usuarios) == enviador.enviar.call_count
 
+
 def test_envio_de_spam(sessao):
     enviador_de_spam = EnviadorDeSpam(sessao, Enviador)
     enviador_de_spam.enviar_emails(
